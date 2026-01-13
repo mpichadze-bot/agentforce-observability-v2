fd99587b00203d573d1e7c7a7b9986f5d731d88f
Maor Pichadze <mpichadze@mpichadz-ltmlh43.internal.salesforce.com>
2026-01-13 18:09:33 +0200

    Add source files and deploy configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/agentforce-observability-v2/',
   server: {
     port: 5180,
     open: true
